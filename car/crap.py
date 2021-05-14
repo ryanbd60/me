54c1f8d8cade9cd83e6aa535d0b4cd472c3a4a1a
ryan s <84178996+ryanbd60@users.noreply.github.com>
2021-05-14 10:32:16 +0100

    Add files via upload

---
+++ car/crap.py
@@ -0,0 +1,11 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>meets</title>
+    <div>
+        <a href="index.html">Home</a>
+        <a href="description.html">About me</a>
+        <a href="meets.html">bit of background</a>
+    </div>
